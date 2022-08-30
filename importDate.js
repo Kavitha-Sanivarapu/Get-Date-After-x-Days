ab3dfb9c52363ebb266a788301268efdaecf2986
Kavitha-Sanivarapu <kavitha.sanivarapu94@gmail.com>
2022-08-30 07:40:52 +0000

    Implementing function to get date after x days

---
+++ importDate.js
@@ -0,0 +1,3 @@
+const no_of_days = require("./index");
+
+console.log(no_of_days(2));
---
+++ index.js
@@ -0,0 +1,9 @@
+const getDateAfterXDays = (days) => {
+  const addDays = require("date-fns/addDays");
+  const result = addDays(new Date(2020, 07, 22), days);
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
+};
+
+module.exports = getDateAfterXDays;
+
+console.log(getDateAfterXDays(2));
